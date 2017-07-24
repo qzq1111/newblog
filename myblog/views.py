b99b41f79250d088f0ed9e8816ad415f9f1ed1a4
qin <472597709@qq.com>
2017-07-24 16:13:15 +0800

    增加主页分页功能

--- myblog/views.py
+++ myblog/views.py
@@ -1,14 +1,22 @@
 #coding:utf-8
 import markdown
 from django.shortcuts import render,redirect
-
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from myblog import models
 # Create your views here.
 
 
 def index(request):
-    posts=models.Post.objects.all()
+    post=models.Post.objects.all().order_by('-created_time')
     sorts=models.Sort.objects.all()
+    paginator=Paginator(post,5)
+    page=request.GET.get('page')
+    try:
+        posts=paginator.page(page)
+    except PageNotAnInteger:
+        posts=paginator.page(1)
+    except EmptyPage:
+        posts=paginator.page(paginator.num_pages)
     return render(request,'index.html',context={'posts':posts,'sorts':sorts})
 def post(request,postid):
     try:
@@ -26,12 +34,11 @@ def sorts(request,sortid):
     try:
         sort=models.Sort.objects.get(id=sortid)
         posts=models.Post.objects.filter(sort=sort)
-        print  posts
     except:
         return  redirect('/')
     return render(request,'sort.html',context={'posts':posts,'sort':sort,})
 
 def search(request):
-    title=request.GET['title']
+    title=request.GET.get('title')
     titles=models.Post.objects.filter(title__contains=title)
     return render(request,'search.html',context={'titles':titles})
