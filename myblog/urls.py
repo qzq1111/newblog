abd5667760f899ffed719f2b2788e436e71bd485
qin <472597709@qq.com>
2017-08-04 19:11:07 +0800

    url设置

---
+++ myblog/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from myblog import views
+app_name='myblog'
+urlpatterns = [
+    url(r'^$',views.index),
+    url(r'^post/(\d+)/$',views.post,name='post_url'),
+    url(r'^category/(\w+)/$',views.category,name='category_url'),
+    url(r'^search/$',views.search,name='search_url'),
+]
