4002e5104bce9009e32046688b2676a891d50fee
qin <472597709@qq.com>
2017-08-04 19:12:24 +0800

    添加评论app

--- newblog/urls.py
+++ newblog/urls.py
@@ -13,13 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
-from myblog import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$',views.index),
-    url(r'^post/(\d+)/$',views.post,name='post_url'),
-    url(r'^category/(\w+)/$',views.category,name='category_url'),
-    url(r'^search/$',views.search,name='search_url'),
+    url(r'',include('myblog.urls')),
+    url(r'',include('comments.urls'))
 ]
