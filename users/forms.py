ca7ebd85db33002e6b3c679467b5137c7bc77c8f
qin <472597709@qq.com>
2017-08-05 21:16:17 +0800

    修改样式

--- users/forms.py
+++ users/forms.py
@@ -5,7 +5,11 @@ from django import forms
 class RegisterForm(UserCreationForm):
     class Meta:
         model=User
-        fields=('username','email')
+        fields=('username','email','password1')
         widgets={'username':forms.TextInput(attrs={'class':'form-control',}),
                  'email':forms.EmailInput(attrs={'class':'form-control',}),
                  }
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
